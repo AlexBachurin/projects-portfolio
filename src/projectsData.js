4c4e7de3c96288d8e50eeb22e065d8546ea3b5a8
Alex <alexbachurin91@gmail.com>
2022-03-22 20:59:30 +0300

    some refactor + display projects from data

---
+++ src/projectsData.js
@@ -0,0 +1,31 @@
+const projectsData = [
+    {
+        id: 1,
+        name: 'commerce store',
+        category: 'react',
+        tags: ['react', 'js', 'html', 'css', 'auth0', 'styled'],
+        imgUrl: 'https://res.cloudinary.com/dljezd6qv/image/upload/v1647971256/PortfolioImages/commerce-store_dfxdpj.jpg',
+        projectUrl: 'https://bacha-react-commerce-store.netlify.app/',
+        gitUrl: 'https://github.com/AlexBachurin/commerce-store'
+    },
+    {
+        id: 2,
+        name: 'github users',
+        category: 'react',
+        tags: ['react', 'js', 'html', 'css', 'auth0'],
+        imgUrl: 'https://res.cloudinary.com/dljezd6qv/image/upload/v1647971261/PortfolioImages/spotify-clone_puaeon.jpg',
+        projectUrl: 'https://bacha-coding-github-users.netlify.app/',
+        gitUrl: 'https://github.com/AlexBachurin/search-github-users'
+    },
+    {
+        id: 3,
+        name: 'spotify clone',
+        category: 'react',
+        tags: ['react', 'js', 'html', 'css'],
+        imgUrl: 'https://res.cloudinary.com/dljezd6qv/image/upload/v1647971259/PortfolioImages/github-users_hhmqsg.jpg',
+        projectUrl: 'https://bacha-spotify.netlify.app/',
+        gitUrl: 'https://github.com/AlexBachurin/spotify-clone-react'
+    }
+]
+
+export default projectsData;
